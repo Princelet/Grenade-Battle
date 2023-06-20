1d1a9dcac65bc332a88383dfc2ac24b0d4bd8911
Princelet <100294262+Princelet@users.noreply.github.com>
2023-06-20 11:08:15 +0100

    Added easing comment

--- GrenadeBattleVS/Easing.cpp
+++ GrenadeBattleVS/Easing.cpp
@@ -1,5 +1,6 @@
 #include "Easing.h"
 
+// Practical Task - Easing Function
 sf::Vector2f Easing::Linear(sf::Vector2f begin, sf::Vector2f change, float duration, float time)
 {
 	return (change / duration) * time + begin;
